14d904cb540f8b6a2a6eeb82a80df9115dcdb1f1
Difegue <sugoi@cock.li>
2019-05-16 23:20:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
+set -e
+
 bash -c  "nohup tail -f /root/.megaCmd/megacmdserver.log >&0 2>1" &
 if [[ "$USERNAME" != "NOBODY" ]] && [[ "$PASSWORD" != "CHANGEME" ]]; then
     mega-login $USERNAME $PASSWORD
-else
-    echo "Please specify a valid username and password with -e USERNAME and -e PASSWORD. Aborting."
-    exit 1
 fi
+
 if [[ "$*" == "" ]]; then
 	echo "Please specify a valid megacli command"
     	exit 1
